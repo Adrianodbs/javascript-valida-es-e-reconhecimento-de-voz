4ac99eafbcc0329afcef4483e680ab594fd0dbea
Adriano <adriano_alves_2008@hotmail.com>
2022-09-26 19:16:30 -0300

    acertando o numero secreto

--- app/validacao.js
+++ app/validacao.js
@@ -3,11 +3,18 @@ function verificaSeOChutePossuiUmValorValido() {
   const numero = +chute
 
   if (chuteForInvalido(numero)) {
-    console.log('valor inválido')
+    elementoChute.innerHTML += '<div>Valor inválido</div>'
   }
 
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-    console.log('valor inválido')
+    elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+     <h2>Você acertou!</h2>
+     <h3>O número secreto era ${numeroSecreto}</h3>
+    `
   }
 
   function numeroForMaiorOuMenorQueOValorPermitido() {
